925f09d0f0649843e1d13621434107e57ad81d2e
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-22 21:34:49 +0530

    First commit

---
+++ Components/Layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Layout() {
+    return <>
+        <Head>
+            
+            <title>SunGlass Ecommerce</title>
+        </Head>
+    </>
+
+}
---
+++ Components/ui/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return <header>
+        <div className="container">
+            
+        </div>
+    </header>
+}
