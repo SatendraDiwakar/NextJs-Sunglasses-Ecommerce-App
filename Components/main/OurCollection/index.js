a2300f647bfc23782a5a0b6620101be473e92986
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-30 22:33:54 +0530

    Update Our collection section

--- Components/main/OurCollection/index.js
+++ Components/main/OurCollection/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 // component
 import Heading from '../../ui/Heading'
 import ButtonBlack from '../../ui/ButtonBlack'
 // style
 import collectionStyle from './Collection.module.css'
+import g2car2 from '../../../public/Images/collection/g2car2.jpg'
 
 export default function OurCollection() {
+
 
     return (
         <section id="OurCollection" className={collectionStyle.collectionSection}>
@@ -14,24 +16,133 @@ export default function OurCollection() {
             <div className={`container ${collectionStyle.container}`}>
                 <div className={collectionStyle.firstGrid}>
                     <div className={collectionStyle.gallery1}>
-                        <img src="/Images/collection/g1col1.jpg" alt="sunglass image collection 1" className={collectionStyle.g1col1} />
-                        <img src="/Images/collection/g1col2.jpg" alt="sunglass image collection 2" className={collectionStyle.g1col2} />
-                        <img src="/Images/collection/g1col3.jpg" alt="sunglass image collection 3" className={collectionStyle.g1col3} />
-                        <img src="/Images/collection/g1col4.jpg" alt="sunglass image collection 4" className={collectionStyle.g1col4} />
-                        <img src="/Images/collection/g1col5.jpg" alt="sunglass image collection 5" className={collectionStyle.g1col5} />
-                        <img src="/Images/collection/g1col6.jpg" alt="sunglass image collection 6" className={collectionStyle.g1col6} />
+                        <div className={collectionStyle.g1col1 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col1.jpg"
+                                alt="sunglass image collection 1"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g1col2 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col2.jpg"
+                                alt="sunglass image collection 2"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g1col3 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col3.jpg"
+                                alt="sunglass image collection 3"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g1col4 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col4.jpg"
+                                alt="sunglass image collection 4"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g1col5 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col5.jpg"
+                                alt="sunglass image collection 5"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g1col6 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g1col6.jpg"
+                                alt="sunglass image collection 6"
+                                layout='fill'
+                                placeholder='blur'
+                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                                priority={true}
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
                     </div>
-                    <img src="/Images/collection/g1car1.jpg" alt="man with sunglass" className={collectionStyle.g1car}/>
+                    <div className={collectionStyle.g1car}>
+                        <Image
+                            src="/Images/collection/g1car1.jpg"
+                            alt="man with sunglass"
+                            layout='fill'
+                            placeholder='blur'
+                            blurDataURL="https://image-component.nextjs.gallery/placeholder"
+                            priority={true}
+                        />
+                    </div>
                 </div>
                 <div className={collectionStyle.secondGrid}>
-                    <img src="/Images/collection/g2car1.jpg" alt="woman with sunglass" className={collectionStyle.g2car1}/>
-                    <div className={collectionStyle.gallery2}>
-                        <img src="/Images/collection/g2col1.jpg" alt="sunglass image collection 7" className={collectionStyle.g2col1} />
-                        <img src="/Images/collection/g2col2.jpg" alt="sunglass image collection 8" className={collectionStyle.g2col2} />
-                        <img src="/Images/collection/g2col3.jpg" alt="sunglass image collection 9" className={collectionStyle.g2col3} />
-                        <img src="/Images/collection/g2col4.jpg" alt="sunglass image collection 10" className={collectionStyle.g2col4} />
+                    <div className={collectionStyle.g2car1}>
+                        <Image
+                            src="/Images/collection/g2car1.jpg"
+                            alt="woman with sunglass 1"
+                            layout='fill'
+                        />
                     </div>
-                    <img src="/Images/collection/g2car2.jpg" alt="woman with sunglass" className={collectionStyle.g2car2}/>
+                    <div className={collectionStyle.gallery2}>
+                        <div className={collectionStyle.g2col1 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g2col1.jpg"
+                                alt="sunglass image collection 7"
+                                layout='fill'
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g2col2 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g2col2.jpg"
+                                alt="sunglass image collection 8"
+                                layout='fill'
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g2col3 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g2col3.jpg"
+                                alt="sunglass image collection 9"
+                                layout='fill'
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                        <div className={collectionStyle.g2col4 + ' ' + collectionStyle.hovEffect}>
+                            <Image
+                                src="/Images/collection/g2col4.jpg"
+                                alt="sunglass image collection 10"
+                                layout='fill'
+                            />
+                            <p className={collectionStyle.price}>$99</p>
+                        </div>
+                    </div>
+                    <div className={collectionStyle.g2car2}>
+                        <Image
+                            src={g2car2}
+                            alt="woman with sunglass 2"
+                            layout='fill'
+                        />
+                    </div>
                 </div>
                 <ButtonBlack name="see all" />
             </div>
