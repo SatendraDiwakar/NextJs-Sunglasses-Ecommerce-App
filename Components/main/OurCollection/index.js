f85a9a9f8b431cbcb83d853f90152cf7727b13f1
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-31 23:06:49 +0530

    Add on loading complete

--- Components/main/OurCollection/index.js
+++ Components/main/OurCollection/index.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
 // component
 import Heading from '../../ui/Heading'
 import ButtonBlack from '../../ui/ButtonBlack'
 // style
 import collectionStyle from './Collection.module.css'
+// images 
+import g1car from '../../../public/Images/collection/g1car1.jpg'
+import g1col1 from '../../../public/Images/collection/g1col1.jpg'
+import g1col2 from '../../../public/Images/collection/g1col2.jpg'
+import g1col3 from '../../../public/Images/collection/g1col3.jpg'
+import g1col4 from '../../../public/Images/collection/g1col4.jpg'
+import g1col5 from '../../../public/Images/collection/g1col5.jpg'
+import g1col6 from '../../../public/Images/collection/g1col6.jpg'
+
+import g2car1 from '../../../public/Images/collection/g2car1.jpg'
 import g2car2 from '../../../public/Images/collection/g2car2.jpg'
+import g2col1 from '../../../public/Images/collection/g2col1.jpg'
+import g2col2 from '../../../public/Images/collection/g2col2.jpg'
+import g2col3 from '../../../public/Images/collection/g2col3.jpg'
+import g2col4 from '../../../public/Images/collection/g2col4.jpg'
 
 export default function OurCollection() {
 
+    function handleHov(className){
+        document.getElementsByClassName(className)[0].classList.add(collectionStyle.hovEffect);
+    }
 
     return (
         <section id="OurCollection" className={collectionStyle.collectionSection}>
@@ -16,122 +33,123 @@ export default function OurCollection() {
             <div className={`container ${collectionStyle.container}`}>
                 <div className={collectionStyle.firstGrid}>
                     <div className={collectionStyle.gallery1}>
-                        <div className={collectionStyle.g1col1 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col1 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col1.jpg"
+                                src={g1col1}
                                 alt="sunglass image collection 1"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col1)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g1col2 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col2 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col2.jpg"
+                                src={g1col2}
                                 alt="sunglass image collection 2"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col2)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g1col3 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col3 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col3.jpg"
+                                src={g1col3}
                                 alt="sunglass image collection 3"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col3)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g1col4 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col4 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col4.jpg"
+                                src={g1col4}
                                 alt="sunglass image collection 4"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col4)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g1col5 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col5 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col5.jpg"
+                                src={g1col5}
                                 alt="sunglass image collection 5"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col5)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g1col6 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g1col6 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g1col6.jpg"
+                                src={g1col6}
                                 alt="sunglass image collection 6"
                                 layout='fill'
                                 placeholder='blur'
-                                blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                                priority={true}
+                                onLoadingComplete={()=>handleHov(collectionStyle.g1col6)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
                     </div>
                     <div className={collectionStyle.g1car}>
                         <Image
-                            src="/Images/collection/g1car1.jpg"
+                            src={g1car}
                             alt="man with sunglass"
                             layout='fill'
                             placeholder='blur'
-                            blurDataURL="https://image-component.nextjs.gallery/placeholder"
-                            priority={true}
                         />
                     </div>
                 </div>
                 <div className={collectionStyle.secondGrid}>
                     <div className={collectionStyle.g2car1}>
                         <Image
-                            src="/Images/collection/g2car1.jpg"
+                            src={g2car1}
                             alt="woman with sunglass 1"
                             layout='fill'
+                            placeholder='blur'
                         />
                     </div>
                     <div className={collectionStyle.gallery2}>
-                        <div className={collectionStyle.g2col1 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g2col1 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g2col1.jpg"
+                                src={g2col1}
                                 alt="sunglass image collection 7"
                                 layout='fill'
+                                placeholder='blur'
+                                onLoadingComplete={()=>handleHov(collectionStyle.g2col1)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g2col2 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g2col2 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g2col2.jpg"
+                                src={g2col2}
                                 alt="sunglass image collection 8"
                                 layout='fill'
+                                placeholder='blur'
+                                onLoadingComplete={()=>handleHov(collectionStyle.g2col2)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g2col3 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g2col3 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g2col3.jpg"
+                                src={g2col3}
                                 alt="sunglass image collection 9"
                                 layout='fill'
+                                placeholder='blur'
+                                onLoadingComplete={()=>handleHov(collectionStyle.g2col3)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
-                        <div className={collectionStyle.g2col4 + ' ' + collectionStyle.hovEffect}>
+                        <div className={collectionStyle.g2col4 + ' ' + collectionStyle.overHide}>
                             <Image
-                                src="/Images/collection/g2col4.jpg"
+                                src={g2col4}
                                 alt="sunglass image collection 10"
                                 layout='fill'
+                                placeholder='blur'
+                                onLoadingComplete={()=>handleHov(collectionStyle.g2col4)}
                             />
                             <p className={collectionStyle.price}>$99</p>
                         </div>
@@ -141,6 +159,7 @@ export default function OurCollection() {
                             src={g2car2}
                             alt="woman with sunglass 2"
                             layout='fill'
+                            placeholder='blur'
                         />
                     </div>
                 </div>
