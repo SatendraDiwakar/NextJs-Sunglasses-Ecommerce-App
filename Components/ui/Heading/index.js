503dfcee923178d683f9462b113d2b6307fcb15e
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-08-31 23:07:34 +0530

    Update Heading component underline div

--- Components/ui/Heading/index.js
+++ Components/ui/Heading/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 // style
 import HeadStyle from './Heading.module.css'
 
-export default function Heading({heading, color}) {
+export default function Heading({ heading, color }) {
 
     return (
         <div className={HeadStyle.headContainer}>
-            <p className={HeadStyle.heading} style={color==='blk'?{color:'#000'}:{color:'#c05858'}}>{heading}</p>
-            <div className={HeadStyle.headLine}></div>
+            <p className={HeadStyle.heading} style={color === 'blk' ? { color: '#000' } : { color: '#c05858' }}>{heading}</p>
+            <div className={HeadStyle.headLineContainer}>
+                <div className={HeadStyle.headLine}></div>
+            </div>
         </div>
     )
 }
