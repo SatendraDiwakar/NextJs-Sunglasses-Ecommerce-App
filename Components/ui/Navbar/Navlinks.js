b0eb1b4d5a2f5c402371863a06e27afb72c56ebb
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-04 22:55:48 +0530

    Add Responsive design to Navbar part 1

--- Components/ui/Navbar/Navlinks.js
+++ Components/ui/Navbar/Navlinks.js
@@ -1,14 +1,70 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Link from 'next/link';
 // styles
 import NavLinkStyle from './Navlinks.module.css';
 
-export default function Navlinks({links}) {
+export default function Navlinks({ links }) {
+
+    const brandLinkArr = ['RayBan', 'Burberry', 'Maui-Jim', 'Gucci']
+
+    function handleLinkClick() {
+        if (window.innerWidth <= 560) {
+            console.log(window.innerWidth);
+            document.getElementById('menuBar').style = 'display: block';
+            document.getElementById('closeBtn').style = 'display: none';
+            document.getElementById('menuLinks').style = 'transform: translate(-100%,-50%)';
+
+        }
+
+    }
+    useEffect(() => {
+        document.getElementById('brandsLink').addEventListener('click', () => {
+            document.getElementsByClassName(NavLinkStyle.brandsList)[0].classList.toggle(NavLinkStyle.anima)
+        })
+    }, []);
+
+    function handleBrandClick(){        
+        document.getElementsByClassName(NavLinkStyle.brandsList)[0].classList.toggle(NavLinkStyle.anima)
+    }
+
     return (
-        <div className={NavLinkStyle.container}>
-            <ul className={NavLinkStyle.list}>
+        <div className={NavLinkStyle.links}>
+
+            <div className={NavLinkStyle.container} id="menuLinks" onClick={handleLinkClick}>
+                <ul className={NavLinkStyle.list}>
+                    <Link href='/' >
+                        <li
+                            className={NavLinkStyle.listItem}
+                            onClick={handleLinkClick}
+                        >home</li>
+                    </Link>
+                    <li
+                        id='brandsLink'
+                        className={NavLinkStyle.listItem}
+                        onClick={handleLinkClick}
+                    >brands</li>
+                    <Link href='/about' >
+                        <li
+                            className={NavLinkStyle.listItem}
+                            onClick={handleLinkClick}
+                        >about us</li>
+                    </Link>
+                    <Link href='/contact' >
+                        <li
+                            className={NavLinkStyle.listItem}
+                            onClick={handleLinkClick}
+                        >contact</li>
+                    </Link>
+                </ul>
+            </div>
+            <ul className={NavLinkStyle.brandsList}>
                 {
-                    links.map((itm,index)=>{
-                        return <li key={itm +'Navlink'+ index} className={NavLinkStyle.listItem}>{itm}</li>
+                    brandLinkArr.map((itm, index) => {
+                        return <Link href={`/brands/${itm}`}><li
+                            key={itm + 'brandLink' + index}
+                            className={NavLinkStyle.brandListItem}
+                            onClick={handleBrandClick}
+                        >{itm}</li></Link>
                     })
                 }
             </ul>
--- Components/ui/Navbar/index.js
+++ Components/ui/Navbar/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 // react-icons
 import { FaOpencart } from 'react-icons/fa';
+import { CgClose } from 'react-icons/cg';
+import { FiMenu } from 'react-icons/fi';
 import { AiOutlineUser } from 'react-icons/ai';
 import { GiSunglasses } from 'react-icons/gi';
 // component
@@ -12,6 +14,33 @@ import NavStyle from './Navbar.module.css';
 export default function Navbar() {
 
     const linkArr = ['home', 'brands', 'about us', 'contact'];
+    const [te, setTe] = useState(false);
+
+    useEffect(() => {
+
+        window.addEventListener('load', () => {
+            if (window.innerWidth > 560) {
+                setTe(false);
+                document.getElementById('menuLinks').style = 'transform: translateX(-50%)';
+            } else {
+                document.getElementsByClassName(NavStyle.menuBar)[0].style = 'display: block';
+                document.getElementsByClassName(NavStyle.closeBtn)[0].style = 'display: none';
+                document.getElementById('menuLinks').style = 'transform: translate(-100%,-50%)';
+                setTe(true);
+            }
+        });
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > 560) {
+                setTe(false);
+                document.getElementById('menuLinks').style = 'transform: translateX(-50%)';
+            } else {
+                document.getElementsByClassName(NavStyle.menuBar)[0].style = 'display: block';
+                document.getElementsByClassName(NavStyle.closeBtn)[0].style = 'display: none';
+                document.getElementById('menuLinks').style = 'transform: translate(-100%,-50%)';
+                setTe(true);
+            }
+        });
+    }, [])
 
     return <header>
         <div className="container">
@@ -26,10 +55,34 @@ export default function Navbar() {
                 </Link>
                 <Navlinks links={linkArr} />
                 <div className={NavStyle.navRight}>
-                    <AiOutlineUser className={NavStyle.user} />
-                    <div className={NavStyle.cartContainer}>
-                        <FaOpencart className={NavStyle.cartIcon} />
-                        <p className={NavStyle.cartText}>cart<span className={NavStyle.itemsNum}>0</span></p>
+                    {
+                        te ? <>
+                            <div className={NavStyle.cartContainer}>
+                                <FaOpencart className={NavStyle.cartIcon} />
+                                <p className={NavStyle.cartText}>cart<span className={NavStyle.itemsNum}>0</span></p>
+                            </div>
+                            <AiOutlineUser className={NavStyle.user} />
+                        </>
+                            :
+                            <>
+                                <AiOutlineUser className={NavStyle.user} />
+                                <div className={NavStyle.cartContainer}>
+                                    <FaOpencart className={NavStyle.cartIcon} />
+                                    <p className={NavStyle.cartText}>cart<span className={NavStyle.itemsNum}>0</span></p>
+                                </div>
+                            </>
+                    }
+                    <div className={NavStyle.menuBtnContainer}>
+                        <FiMenu className={NavStyle.menuBar} id='menuBar' onClick={() => {
+                            document.getElementsByClassName(NavStyle.menuBar)[0].style = 'display: none';
+                            document.getElementsByClassName(NavStyle.closeBtn)[0].style = 'display: block';
+                            document.getElementById('menuLinks').style = 'transform: translate(0,-50%)';
+                        }} />
+                        <CgClose className={NavStyle.closeBtn} id='closeBtn' onClick={() => {
+                            document.getElementsByClassName(NavStyle.menuBar)[0].style = 'display: block';
+                            document.getElementsByClassName(NavStyle.closeBtn)[0].style = 'display: none';
+                            document.getElementById('menuLinks').style = 'transform: translate(-100%,-50%)';
+                        }} />
                     </div>
                 </div>
             </nav>
