f73f80b639545e9a270fcd6eaeba4ee6a9330afe
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-16 00:06:01 +0530

    Update file names in import of models

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,12 +1,12 @@
 import nc from 'next-connect'
 import db from '../../../utils/db';
-import Product from '../../../models/Product';
+import ProductModel from '../../../models/ProductModelModel';
 
 const handler = nc();
 
 handler.get(async (req,res)=>{
     await db.connect();
-    const products = await Product.find({});
+    const products = await ProductModel.find({});
     await db.disconnect();
     res.send(products);
 })
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,21 +1,21 @@
 import nc from 'next-connect'
 import db from '../../utils/db';
-import Product from '../../models/Product';
-import HeroPic from '../../models/Heropics';
+import HeroPicModel from '../../models/HeropicsModel'
+import ProductModel from '../../models/ProductModel'
 import data from '../../utils/data';
 
 const handler = nc();
 
 handler.get(async (req,res)=>{
     await db.connect();
-    await Product.deleteMany();
-    await Product.insertMany([
+    await ProductModel.deleteMany();
+    await ProductModel.insertMany([
         ...data.products.home.topSeller,
         ...data.products.home.sale,
         ...data.products.home.collection,
     ]);
-    await HeroPic.deleteMany();
-    await HeroPic.create(data.products.home.character);
+    await HeroPicModel.deleteMany();
+    await HeroPicModel.create(data.products.home.character);
     await db.disconnect();
     res.send({message: 'seeded successfully'});
 })
