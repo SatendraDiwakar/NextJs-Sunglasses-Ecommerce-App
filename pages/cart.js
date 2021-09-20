5ec9951be5119723a72e8b23b6f6e0738415298d
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-20 23:20:15 +0530

    Add cart page

---
+++ pages/cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// component
+import ShoppingCart from '../Components/main/ShoppingCart'
+
+export default function Cart() {
+    return (
+        <>
+            <ShoppingCart />
+        </>
+    )
+}
