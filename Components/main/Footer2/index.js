7fa130c90598efff7b8da6dba55817c2a372be32
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-23 00:29:31 +0530

    Change Position Styling logic

--- Components/main/Footer2/index.js
+++ Components/main/Footer2/index.js
@@ -8,51 +8,49 @@ import Footer2Style from './Footer2.module.css'
 export default function Footer2() {
 
     const router = useRouter();
-    const [stylState, setStylState] = useState(false);
-    const [stylState2, setStylState2] = useState(true);
-    const [heightOfCart, setHeightOfCart] = useState(0);
-    const foo = useRef(null);
-    
+    // state
+    const [footer2Color, setFooter2Color] = useState(false);
+    const [footer2Pos, setFooter2Pos] = useState(true);
+
     // context
     const context = useContext(StoreCtx);
     const { state } = context;
 
-    const styl = {
+    const stylF2color = {
         background: '#160514',
         color: 'rgba(255, 255, 255, 0.75)'
     }
-    const styl2 = {
+    const stylF2Pos = {
         position: 'absolute',
         bottom: '0',
     }
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            let shoppingCart = document.getElementById('shoppingCartSection').getBoundingClientRect().bottom;
-            setHeightOfCart(shoppingCart);
-        })        
-    },[])
-
     useEffect(() => {
+        var body = document.body,
+            html = document.documentElement;
+
+        var height = Math.max(body.scrollHeight, body.offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight);
+
         if (document) {
             if (router.pathname === '/') {
-                setStylState(true);
+                setFooter2Color(true);
             } else if (router.pathname === '/cart') {
-                    if (heightOfCart >= window.innerHeight) {
-                        setStylState2(false)
-                    } else {
-                        setStylState2(true)
-                    }
-                setStylState(false);
+                if (height <= window.innerHeight) {
+                    setFooter2Pos(true)
+                } else {
+                    setFooter2Pos(false)
+                }
+                setFooter2Color(false);
             } else {
-                setStylState(false);
+                setFooter2Color(false);
             }
         }
     }, [router.pathname, state.cart.cartItems.length])
 
     return (
-        <div ref={foo} className={Footer2Style.container} style={stylState ? styl : stylState2 ? styl2 : null}>
-            <p className={Footer2Style.rights} style={stylState ? styl : null}>All rights reserved. Satendra Sunglass Ecomm.</p>
+        <div className={Footer2Style.container} style={footer2Color ? stylF2color : footer2Pos ? stylF2Pos : null}>
+            <p className={Footer2Style.rights} style={footer2Color ? stylF2color : null}>All rights reserved. Satendra Sunglass Ecomm.</p>
         </div>
     )
 }
