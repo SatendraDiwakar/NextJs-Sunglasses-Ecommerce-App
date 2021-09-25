1e35389ae3d287b014a6fd4a74281b8b4fe92791
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-26 00:25:45 +0530

    Built Register api

---
+++ pages/api/users/register.js
@@ -0,0 +1,32 @@
+import nc from 'next-connect'
+import bcrypt from 'bcryptjs'
+import db from '../../../utils/db';
+import UserModel from '../../../models/UserModel';
+import { signToken } from '../../../utils/auth';
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+    await db.connect();
+    const newUser = new UserModel({
+        name: req.body.userName,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    await db.disconnect();
+    console.log(user);
+
+    const token = signToken(user);
+
+    res.send({
+        token,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    })
+
+})
+
+export default handler;
