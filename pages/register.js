d70370d6fa2b971237b85fa1f4fad84b6de9daba
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-26 00:24:55 +0530

    Built Registration page

---
+++ pages/register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// component
+import RegisterComp from '../Components/main/RegisteComp'
+
+export default function Login() {
+    return (
+        <RegisterComp />
+    )
+}
