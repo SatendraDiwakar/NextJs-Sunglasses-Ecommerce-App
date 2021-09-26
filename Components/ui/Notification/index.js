6dc7ef3777e96ee909a6df21836c9aa224fffd5a
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-27 00:42:55 +0530

    Complete Notification component

--- Components/ui/Notification/index.js
+++ Components/ui/Notification/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+// context
+import { NotifyCtx } from '../../../utils/NotifyCtx'
 // style
 import NotificationStyle from './Notification.module.css'
 
-export default function Notification({ message, type, closeNotification }) {
+export default function Notification({ message, type }) {
+
+    const context = useContext(NotifyCtx);
+    const { showNotification, hide } = context;
 
     return (
-        <div className={NotificationStyle.container} style={{ background: type === 'error' ? 'red' : 'green' }}>
+        <div className={NotificationStyle.container} style={type === 'error' ? { background: 'red', color: '#fff' } : { background: '#00ff82' }}>
             <p>{message}</p>
-            <button className={NotificationStyle.closeBtn} onClick={closeNotification}>X</button>
+            <button className={NotificationStyle.closeBtn} onClick={hide}>X</button>
         </div>
     )
 }
