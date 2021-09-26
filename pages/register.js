437d38572d653368044b01c00c560ef946143c66
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-27 00:56:41 +0530

    Add RegisterComp to register page

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // component
-import RegisterComp from '../Components/main/RegisteComp'
+import RegisterComp from '../Components/main/RegisterComp'
 
 export default function Login() {
     return (
