dd92182060b96c99a0e1defc3bea40d39ac3f20a
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-28 01:31:26 +0530

    Complete checkout wizard component

--- Components/main/CheckoutWiz/index.js
+++ Components/main/CheckoutWiz/index.js
@@ -1,48 +1,73 @@
 import React from 'react'
 // material ui
-import { Stepper, Step, StepLabel, Box } from '@material-ui/core'
+import { Stepper, Step, StepLabel, StepConnector } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 // style
 import CheckoutWizStyle from './CheckoutWiz.module.css'
 
 export default function CheckoutWiz({ activeStep = 0 }) {
+
     const steps = ['Login', 'Shipping Address', 'Payment Method', 'Place Order']
-    return (
+
+    const useStyle = makeStyles({
+        stepperBackground: {
+            background: 'transparent'
+        },
+        stepIconTextFontSize: {
+            fontSize: '1.2rem',
+        },
+        stepIconRoot: {
+            fontSize: 25,
+            "&.MuiStepIcon-active": {
+                color: "#0087ff"
+            },
+            "&.MuiStepIcon-completed": {
+                color: "#da5252"
+            }
+        },
+        stepLabelStyles: {
+            fontSize: '1.5rem',
+            "&.MuiStepLabel-completed": {
+                fontWeight: 600,
+                color: "#da5252"
+            },
+            "&.MuiStepLabel-active": {
+                fontWeight: 600,
+                color: "#0087ff"
+            },
+        },
+        stepConnectorStyle: {
+            "&.MuiStepConnector-active": {
+                background: '#0087ff'
+            },
+            "&.MuiStepConnector-completed": {
+                background: '#ff1010'
+            },
+        }
+    });
+    const checkoutWizMuiStyle = useStyle();
+
+    return (<>
         <div className={CheckoutWizStyle.container}>
-                <Stepper className={CheckoutWizStyle.trans} activeStep={1} alternativeLabel>
-                    {steps.map((label) => (
-                        <Step key={label}>
-                            <StepLabel>{label}</StepLabel>
-                        </Step>
-                    ))}
-                </Stepper>
-            {/* <div className={CheckoutWizStyle.line}>
-            </div>
-            <div className={CheckoutWizStyle.stepsContainer}>
-                <div className={CheckoutWizStyle.step}>
-                    <div className={CheckoutWizStyle.stepIcon}>
-
-                    </div>
-                    <p className={CheckoutWizStyle.stepName}>Login</p>
-                </div>
-                <div className={CheckoutWizStyle.step}>
-                    <div className={CheckoutWizStyle.stepIcon}>
-
-                    </div>
-                    <p className={CheckoutWizStyle.stepName}>Shipping Address</p>
-                </div>
-                <div className={CheckoutWizStyle.step}>
-                    <div className={CheckoutWizStyle.stepIcon}>
-
-                    </div>
-                    <p className={CheckoutWizStyle.stepName}>Payment</p>
-                </div>
-                <div className={CheckoutWizStyle.step}>
-                    <div className={CheckoutWizStyle.stepIcon}>
-
-                    </div>
-                    <p className={CheckoutWizStyle.stepName}>Login</p>
-                </div>
-            </div> */}
+            <Stepper
+                activeStep={activeStep}
+                classes={{ root: checkoutWizMuiStyle.stepperBackground }}
+                connector={<StepConnector classes={{ root: checkoutWizMuiStyle.stepConnectorStyle }} />}
+                alternativeLabel
+            >
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel classes={{ label: checkoutWizMuiStyle.stepLabelStyles }}
+                            StepIconProps={{
+                                classes: {
+                                    root: checkoutWizMuiStyle.stepIconRoot,
+                                    text: checkoutWizMuiStyle.stepIconTextFontSize,
+                                }
+                            }}>
+                            {label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
         </div>
-    )
+    </>)
 }
