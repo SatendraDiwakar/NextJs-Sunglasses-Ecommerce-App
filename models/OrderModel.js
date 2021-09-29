6146e548170f19db01351089da0beb6dc551e08c
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-09-30 00:29:36 +0530

    Create: Order Model

---
+++ models/OrderModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    orderItems: [
+        {
+            prodName: { type: String, required: true },
+            prodQuantity: { type: Number, required: true },
+            prodImage: { type: String, required: true },
+            prodPrice: { type: Number, required: true },
+        },
+    ],
+    shippingAddress: {
+        fullName: { type: String, required: true },
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    payMethod: { type: String, required: true },
+    // paymentResult: { id: String, status: String, email_address: String },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    // isPaid: { type: Boolean, required: true, default: false },
+    // isDelivered: { type: Boolean, required: true, default: false },
+    // paidAt: { type: Date },
+    // deliveredAt: { type: Date },
+}, {
+    timestamps: true
+});
+
+const OrderModel = mongoose.models.Order || mongoose.model('Order', orderSchema);
+
+export default OrderModel;
