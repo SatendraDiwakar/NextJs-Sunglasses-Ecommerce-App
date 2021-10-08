19fb32ce77248d6e2ee1a09b826353c3c4221b40
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-10-09 00:52:30 +0530

    Change padding style

--- Components/ui/Loader2/index.js
+++ Components/ui/Loader2/index.js
@@ -4,7 +4,7 @@ import Loader2Style from './Loader2.module.css'
 
 export default function Loader2() {
     return (
-        <div style={{padding: '1rem'}}>
+        <div style={{padding: '4rem 0'}}>
             <div className={Loader2Style.loader + ' ' + Loader2Style.rotate} />
         </div>
     )
