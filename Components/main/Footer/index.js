20355392e426976e5c765e1b1500e927812c4983
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-10-10 00:27:51 +0530

    Add Footer buttons functionality

--- Components/main/Footer/index.js
+++ Components/main/Footer/index.js
@@ -1,31 +1,60 @@
 import React from 'react'
 // react icons
 import { FaTwitter, FaPinterest, FaFacebook, FaMapMarkedAlt } from 'react-icons/fa'
+// router
+import { useRouter } from 'next/router';
 //style
 import footerStyle from './Footer.module.css'
 
 export default function Footer() {
+
+    const router = useRouter();
+
     return (
         <section id="footer" className={footerStyle.footerSection}>
             <div className={`container ${footerStyle.container}`}>
                 <div className={`${footerStyle.item} ${footerStyle.shop}`}>
                     <h3>Shop</h3>
-                    <p className={footerStyle.footerText}>RayBan</p>
-                    <p className={footerStyle.footerText}>Gucci</p>
-                    <p className={footerStyle.footerText}>Maui Jim</p>
-                    <p className={footerStyle.footerText}>Burberry</p>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/brands/RayBan')}
+                    >RayBan</button>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/brands/Gucci')}
+                    >Gucci</button>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/brands/Maui-Jim')}
+                    >Maui Jim</button>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/brands/Burberry')}
+                    >Burberry</button>
                 </div>
                 <div className={`${footerStyle.item} ${footerStyle.company}`}>
                     <h3>Company</h3>
-                    <p className={footerStyle.footerText}>About Us</p>
-                    <p className={footerStyle.footerText}>Contact Us</p>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/about')}
+                    >About Us</button>
+                    <button
+                        style={{ background: 'transparent', border: 'none' }}
+                        className={footerStyle.footerText}
+                        onClick={() => router.push('/contact')}
+                    >Contact Us</button>
                 </div>
                 <div className={`${footerStyle.item} ${footerStyle.social}`}>
                     <h3>Get Social</h3>
                     <div className={footerStyle.socialIconsContainer}>
-                        <FaTwitter className={footerStyle.scIcons} />
-                        <FaPinterest className={footerStyle.scIcons} />
-                        <FaFacebook className={footerStyle.scIcons} />
+                        <FaTwitter className={footerStyle.scIcons} onClick={() => window.scrollTo(0, 0)} />
+                        <FaPinterest className={footerStyle.scIcons} onClick={() => window.scrollTo(0, 0)} />
+                        <FaFacebook className={footerStyle.scIcons} onClick={() => window.scrollTo(0, 0)} />
                     </div>
                 </div>
                 <div className={`${footerStyle.item} ${footerStyle.location}`}>
